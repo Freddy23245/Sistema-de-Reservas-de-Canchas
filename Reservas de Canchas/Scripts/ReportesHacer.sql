b965a02874718cb444b2c6bc450b7e44bc9e9613
Freddy23245 <cyberdyne710@gmail.com>
2025-06-01 16:35:07 -0300

    cambio1.0

--- Reservas de Canchas/Scripts/ReportesHacer.sql
+++ Reservas de Canchas/Scripts/ReportesHacer.sql
@@ -19,10 +19,12 @@
 						WHERE YEAR(Fecha)=2025
 			GROUP BY DATEPART(MONTH, Fecha)
 			ORDER BY DATEPART(MONTH, Fecha) ASC
-
+GO
 
 -- cancha mas reservada por mes
-
+CREATE PROCEDURE CanchaMasReservada 
+@anio INT
+as
 	SELECT
 			CASE
 			WHEN DATEPART(MONTH,Fecha)=1 THEN 'Enero'
@@ -42,29 +44,36 @@
 			COUNT(C.Id_Cancha)AS [Cantidad],C.Numero, C.Precio
 			FROM Reservas R
 			INNER JOIN Canchas C on R.Id_Cancha = C.Id_Cancha
-						WHERE YEAR(Fecha)=2025
+						WHERE YEAR(Fecha)=@anio 
 			GROUP BY DATEPART(MONTH, Fecha),C.Numero,C.Precio
 			ORDER BY DATEPART(MONTH, Fecha) ASC
+GO
 
 
 -- Horario con mas reservas por mes
 
-
+CREATE PROC HorarioMasReservasPorMes
+@anio int,
+@mes int
+as
 SELECT
-    H.Hora,
+	H.Hora,
     COUNT(R.Id_Horario) AS TotalReservas
 FROM Horarios H
 LEFT JOIN Reservas R
-    ON H.Id_Horario = R.Id_Horario AND YEAR(R.Fecha) = 2025 AND MONTH(R.Fecha) = 5
+    ON H.Id_Horario = R.Id_Horario AND YEAR(R.Fecha) = @anio AND MONTH(R.Fecha) = @mes
 GROUP BY H.Hora, DATEPART(MONTH, R.Fecha)
 ORDER BY H.Hora ASC
-
+GO
 
 -- Clientes con mas reservas
-
+CREATE PROCEDURE ClientesConMasReservas
+@anio int
+as
 SELECT CONCAT(C.Nombre, ',',C.Apellido) AS Cliente, COUNT(R.Id_Cliente) AS Cantidad
 FROM Clientes C
 INNER JOIN Reservas R
 ON C.Id_Cliente =R.Id_Cliente
-WHERE YEAR(R.Fecha)=2025 --AND  MONTH(R.Fecha) = 5
+WHERE YEAR(R.Fecha)=@anio 
 GROUP BY DATEPART(MONTH, R.Fecha),C.Nombre,C.Apellido
+GO
